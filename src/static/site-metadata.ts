2b4703bbc9089a03df94dcb33a1017f68b52a810
bowen-acui <qq384504324@gmail.com>
2026-05-16 16:51:39 +0400

    Customize running page for A Cui

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,38 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const getBasePath = () => {
+  const baseUrl = import.meta.env.BASE_URL;
+  return baseUrl === '/' ? '' : baseUrl;
+};
+
+const data: ISiteMetadataResult = {
+  siteTitle: '阿崔 Running',
+  siteUrl: 'https://github.com/bowen-acui/running-page',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Summary',
+      url: `${getBasePath()}/summary`,
+    },
+    {
+      name: 'Blog',
+      url: 'https://github.com/bowen-acui/running-page',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/bowen-acui/running-page/blob/main/README-CN.md',
+    },
+  ],
+};
+
+export default data;
